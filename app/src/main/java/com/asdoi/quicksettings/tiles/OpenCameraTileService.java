2cbaecd1d830949c8acbb51e343c5521c9876898
asdoi <asdoi@t-online.de>
2020-09-05 03:00:03 +0000

    add Open Camera

--- app/src/main/java/com/asdoi/quicksettings/tiles/OpenCameraTileService.java
+++ app/src/main/java/com/asdoi/quicksettings/tiles/OpenCameraTileService.java
@@ -1,17 +1,17 @@
 package com.asdoi.quicksettings.tiles;
 
 import android.content.Intent;
-import android.provider.AlarmClock;
+import android.provider.MediaStore;
 
 import com.asdoi.quicksettings.utils.BaseTileService;
 
-public class NewTimerTileService extends BaseTileService {
+public class OpenCameraTileService extends BaseTileService {
 
     @Override
     public void onClick() {
-        Intent new_timer = new Intent(AlarmClock.ACTION_SET_TIMER);
-        new_timer.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(new_timer);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
         Intent close_notification_bar = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
         sendBroadcast(close_notification_bar);
     }

8cf239d747a8d1f748f70455eea1b2912da7d1e3
Felix Hollederer <flxholle@posteo.com>
2025-06-08 17:02:29 +0200

    fix intent tiles

--- app/src/main/java/com/flxholle/quicktiles/abstract_tiles/IntentTileService.java
+++ app/src/main/java/com/flxholle/quicktiles/abstract_tiles/IntentTileService.java
@@ -1,6 +1,8 @@
 package com.flxholle.quicktiles.abstract_tiles;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.widget.Toast;
 
 import com.flxholle.quicktiles.R;
@@ -12,7 +14,17 @@ public abstract class IntentTileService extends BaseTileService {
         Intent intent = createIntent();
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityAndCollapse(intent);
+            if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                startActivityAndCollapse(intent);
+            } else {
+                PendingIntent pendingIntent = PendingIntent.getActivity(
+                        this,
+                        0,
+                        intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+                );
+                startActivityAndCollapse(pendingIntent);
+            }
         } else {
             Toast.makeText(this, R.string.no_app_found, Toast.LENGTH_LONG).show();
         }
